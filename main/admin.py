326d6a6b1e4325faf5698e43d7db7ac3b4d83c57
kimjunheyon <rlawnsgus9545@naver.com>
2023-12-07 13:24:11 +0900

    모델 망함 2

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# main/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User_information_table
+
+class CustomUserAdmin(UserAdmin):
+    model = User_information_table
+
+admin.site.register(User_information_table, CustomUserAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User_information_table(AbstractUser):
+    locker = models.BooleanField(default=False)
+    lockerpw = models.IntegerField(default=0)
+    PT = models.BooleanField(default=False)
+    PT_starttime = models.DateTimeField(auto_now=False, null=True, blank=True)
+    PT_endtime = models.DateTimeField(auto_now=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.username  
